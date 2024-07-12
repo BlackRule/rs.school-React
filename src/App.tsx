8756c68c132e67327d624d6b447545a12b0af343
Your Name <you@example.com>
2024-07-12 13:30:00 +0200

    feat: All components must be changed to functional components, except Error Boundary components, as error boundaries in React still need to be class components. feat: Implement custom hook to restore search query from the local storage (LS) and put it to the LS on unmount. Use respective React lifecycle hook as a basis.

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,53 @@
-import { Component, ReactNode } from "react";
+import { ReactNode, useCallback, useEffect, useState } from "react";
 
-class Button extends Component<{ children: ReactNode; onClick: () => void }> {
-  render() {
-    return (
-      <button
-        className="bg-gray-300 active:bg-gray-200 hover:bg-gray-100 p-1"
-        onClick={this.props.onClick}
-      >
-        {this.props.children}
-      </button>
-    );
-  }
+function Button(props: { children: ReactNode; onClick: () => void }) {
+  return (
+    <button
+      className="bg-gray-300 active:bg-gray-200 hover:bg-gray-100 p-1"
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 }
 
-class Search extends Component<
-  { onClickOrMount: (value: string) => void },
-  { value: string }
-> {
-  static VALUE_KEY = "search_term";
-
-  state = { value: "" };
-
-  componentDidMount() {
-    let v = localStorage.getItem(Search.VALUE_KEY);
+function useSearchQuery(valueKey: string) {
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    let v = localStorage.getItem(valueKey);
     if (v === null) v = "";
-    this.setState({ value: v });
-    this.props.onClickOrMount(v);
-  }
+    setValue(v);
+    return () => localStorage.setItem(valueKey, value);
+  }, [value, valueKey]);
+  return [value, setValue] as const;
+}
 
-  onClickOrMount = () => {
-    this.props.onClickOrMount(this.state.value);
-    localStorage.setItem(Search.VALUE_KEY, this.state.value);
-  };
-
-  render() {
-    return (
-      <div className="flex gap-1">
-        <input
-          type="text"
-          className="border"
-          placeholder="search query"
-          value={this.state.value}
-          onChange={(e) => this.setState({ value: e.target.value })}
-        />
-        <Button onClick={this.onClickOrMount}>Search</Button>
-      </div>
-    );
-  }
+function Search({
+  onClickOrMount,
+}: {
+  onClickOrMount: (value: string) => void;
+}) {
+  const VALUE_KEY = "search_term";
+  const [value, setValue] = useSearchQuery(VALUE_KEY);
+  const onClick = useCallback(() => {
+    onClickOrMount(value);
+    localStorage.setItem(VALUE_KEY, value);
+  }, [value, onClickOrMount]);
+  useEffect(() => {
+    onClickOrMount(value);
+  }, [onClickOrMount, value]);
+  return (
+    <div className="flex gap-1">
+      <input
+        type="text"
+        className="border"
+        placeholder="search query"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Button onClick={onClick}>Search</Button>
+    </div>
+  );
 }
 
 type Product = {
@@ -55,58 +56,44 @@ type Product = {
   id: number;
 };
 
-class Items extends Component<{
-  items: null | Product[];
-}> {
-  render() {
-    if (this.props.items === null) return <div>loader</div>;
-    return (
-      <div className="flex flex-col gap-2">
-        {this.props.items.map((item) => (
-          <div className="border-2" key={item.id}>
-            <div>{item.title}</div>
-            <div>{item.description}</div>
-          </div>
-        ))}
-      </div>
-    );
-  }
+function Items(props: { items: null | Product[] }) {
+  if (props.items === null) return <div>loader</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      {props.items.map((item) => (
+        <div className="border-2" key={item.id}>
+          <div>{item.title}</div>
+          <div>{item.description}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-class App extends Component {
-  state = {
-    items: null as Product[] | null,
-  };
-  onClickOrMountSearch = async (v: string) => {
+function App() {
+  const [items, setItems] = useState<Product[] | null>(null);
+  const onClickOrMountSearch = useCallback(async (v: string) => {
     v = v.trim();
     const response = (await fetch(
       `https://dummyjson.com/products/search?select=title,description,number&q=${v}`,
     ).then((res) => res.json())) as {
       products: [Product];
-      total: number;
-      skip: number;
-      limit: number;
     };
-    this.setState({
-      items: response.products,
-    });
-  };
+    setItems(response.products);
+  }, []);
 
-  onClickError = () => null;
+  return (
+    <div className="p-6 flex flex-col gap-2">
+      <div className="flex gap-5">
+        <Search onClickOrMount={onClickOrMountSearch} />
 
-  render() {
-    return (
-      <div className="p-6 flex flex-col gap-2">
-        <div className="flex gap-5">
-          <Search onClickOrMount={this.onClickOrMountSearch} />
-          <Button onClick={() => this.setState({ items: 8 })}>
-            Throw an error
-          </Button>
-        </div>
-        <Items items={this.state.items} />
+        <Button onClick={() => setItems(8 as unknown as null)}>
+          Throw an error
+        </Button>
       </div>
-    );
-  }
+      <Items items={items} />
+    </div>
+  );
 }
 
 export default App;
